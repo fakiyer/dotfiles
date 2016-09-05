297bd1da76e0f1f40f7d5766b95a8d56de6cdc74
fakiyer <xmnwt558@yahoo.co.jp>
2016-09-05 16:03:56 +0900

    add karabiner setting

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.atom ~/.atom
-sh ~/dotfiles/karabiner-import.sh
+ln -sf ~/dotfiles/karabiner/private.xml ~/Library/Application\ Support/Karabiner/private.xml
+sh ~/dotfiles/karabiner/import.sh
 sh ~/dotfiles/brew.sh
