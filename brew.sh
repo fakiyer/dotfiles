631508b62446a9be06bc543680189a2c82224468
fakiyer <xmnwt558@yahoo.co.jp>
2016-09-05 12:41:33 +0900

    add brew setting

---
+++ brew.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Install command-line tools using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade --all
+
+# Install Zsh
+# Note: don’t forget to add `/usr/local/bin/zsh` to `/etc/shells` before
+# running `chsh`.
+brew install --without-etcdir zsh
+
+# Switch to using brew-installed zsh as default shell
+if ! fgrep -q '/usr/local/bin/zsh' /etc/shells; then
+  echo '/usr/local/bin/zsh' | sudo tee -a /etc/shells;
+  chsh -s /usr/local/bin/zsh;
+fi;
+
+# Install more recent versions of some macOS tools.
+brew install vim
+
+# Install other useful binaries.
+brew install ghq
+brew install git
+brew install git-lfs
+brew install reattach-to-user-namespace
+brew install tmux
+brew install tree
+
+# Remove outdated versions from the cellar.
+brew cleanup
--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
-#! /bin/bash
+#!/bin/bash
 
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.atom ~/.atom
 sh ~/dotfiles/karabiner-import.sh
+sh ~/dotfiles/brew.sh
