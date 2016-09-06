8507cb7d3b9096ec4c1fff8ace101a7556f768c2
fakiyer <xmnwt558@yahoo.co.jp>
2016-09-06 12:31:52 +0900

    add git setting

--- install.sh
+++ install.sh
@@ -5,5 +5,8 @@ ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.atom ~/.atom
 ln -sf ~/dotfiles/karabiner/private.xml ~/Library/Application\ Support/Karabiner/private.xml
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
+
 sh ~/dotfiles/karabiner/import.sh
 sh ~/dotfiles/brew.sh
