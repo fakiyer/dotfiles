06b56b5bc5555a931673e474fb5864121f78f280
fakiyer <xmnwt558@yahoo.co.jp>
2017-02-28 17:18:09 +0900

    change key repeat

--- karabiner/import.sh
+++ karabiner/import.sh
@@ -4,8 +4,8 @@ cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
 $cli set remap.mouse_button4_5 1
 /bin/echo -n .
-$cli set repeat.initial_wait 300
+$cli set repeat.initial_wait 200
 /bin/echo -n .
-$cli set repeat.wait 33
+$cli set repeat.wait 20
 /bin/echo -n .
 /bin/echo
