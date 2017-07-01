be76528b87585c8e9005353a316a93e5dfbdfa67
fakiyer <xmnwt558@yahoo.co.jp>
2017-07-01 15:14:57 +0900

    setting ctags

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.atom ~/.atom
 ln -sf ~/dotfiles/karabiner/private.xml ~/Library/Application\ Support/Karabiner/private.xml
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.git_template ~/.git_template
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 
 sh ~/dotfiles/karabiner/import.sh
