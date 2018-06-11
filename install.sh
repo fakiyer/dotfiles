24d59c9933e20009b5e22b2d2f8c0f43faa5b22b
fakiyer <fakiyer@gmail.com>
2018-06-11 10:11:25 +0900

    add vscode keybindings

--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@ ln -sf ~/dotfiles/.git_template ~/.git_template
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/.ripgreprc ~/.ripgreprc
 ln -s ~/dotfiles/VSCode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -s ~/dotfiles/VSCode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 ln -s ~/dotfiles/VSCode/snippets/ ~/Library/Application\ Support/Code/User/snippets
 
 #sh ~/dotfiles/karabiner/import.sh
