50c23c92d4727ac0e7272ca20b252a41b4670d4e
fakiyer <fakiya.pepabo@gmail.com>
2018-11-08 16:00:40 +0900

    Add pip install command

--- brew.sh
+++ brew.sh
@@ -11,6 +11,11 @@ brew upgrade
 # Install all our dependencies with bundle (See Brewfile)
 brew bundle
 
+pip2 install neovim
+pip3 install neovim
+pip2 install pip-review
+pip3 install pip-review
+
 # Switch to using brew-installed zsh as default shell
 if ! fgrep -q '/usr/local/bin/zsh' /etc/shells; then
   echo '/usr/local/bin/zsh' | sudo tee -a /etc/shells;
