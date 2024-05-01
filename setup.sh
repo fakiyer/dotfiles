a9cbdbe86bc41c0d28d27fb050d9135e0d319828
fakiyer <fakiyer@gmail.com>
2024-05-01 17:50:18 +0900

    fix homebrew install

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 sh ~/dotfiles/brew.sh
