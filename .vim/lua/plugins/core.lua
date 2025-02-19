1f9ee2dcaf9cc1476e90c0410cafc66aa25ac914
fakiyer <fakiyer@gmail.com>
2025-02-19 13:48:52 +0900

    add quicker.nvim

--- .vim/lua/plugins/core.lua
+++ .vim/lua/plugins/core.lua
@@ -151,4 +151,39 @@ let g:test#custom_transformations = {'docker': function('DockerTransformer')}
     "chr4/nginx.vim",
     ft = { "nginx" }
   },
+  {
+    'stevearc/quicker.nvim',
+    event = "FileType qf",
+    opts = {},
+    config = function()
+      vim.keymap.set("n", "<leader>q", function()
+        require("quicker").toggle()
+      end, {
+        desc = "Toggle quickfix",
+      })
+      vim.keymap.set("n", "<leader>l", function()
+        require("quicker").toggle({ loclist = true })
+      end, {
+        desc = "Toggle loclist",
+      })
+      require("quicker").setup({
+        keys = {
+            {
+              ">",
+              function()
+                require("quicker").expand({ before = 2, after = 2, add_to_existing = true })
+              end,
+              desc = "Expand quickfix context",
+            },
+            {
+              "<",
+              function()
+                require("quicker").collapse()
+              end,
+              desc = "Collapse quickfix context",
+            },
+          },
+      })
+    end,
+  }
 }
